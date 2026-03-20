6b21a12b342d4b6ba1319bebefdf39858905f05e
Allen Shaw <allen@JoineryHQ.com>
2026-03-20 18:21:20 -0500

    civix upgrade, format: 25.10.2

--- cpptmembership.php
+++ cpptmembership.php
@@ -300,15 +300,6 @@ function cpptmembership_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function cpptmembership_civicrm_xmlMenu(&$files) {
-  _cpptmembership_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
@@ -318,114 +309,12 @@ function cpptmembership_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function cpptmembership_civicrm_postInstall() {
-  _cpptmembership_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function cpptmembership_civicrm_uninstall() {
-  _cpptmembership_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function cpptmembership_civicrm_enable() {
   _cpptmembership_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function cpptmembership_civicrm_disable() {
-  _cpptmembership_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function cpptmembership_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _cpptmembership_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function cpptmembership_civicrm_managed(&$entities) {
-  _cpptmembership_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function cpptmembership_civicrm_caseTypes(&$caseTypes) {
-  _cpptmembership_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function cpptmembership_civicrm_angularModules(&$angularModules) {
-  _cpptmembership_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function cpptmembership_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _cpptmembership_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function cpptmembership_civicrm_entityTypes(&$entityTypes) {
-  _cpptmembership_civix_civicrm_entityTypes($entityTypes);
-}
-
-/**
- * Implements hook_civicrm_thems().
- */
-function cpptmembership_civicrm_themes(&$themes) {
-  _cpptmembership_civix_civicrm_themes($themes);
 }
 
 /**
